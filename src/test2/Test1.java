0b3aed0343a0a1f440721f95af4222ae2922a16e
yangyang <2324654129@qq.com>
2020-06-26 13:27:17 +0800

    bbbbbbss

--- src/test2/Test1.java
+++ src/test2/Test1.java
@@ -1,4 +1,9 @@
 package test2;
 
 public class Test1 {
+    public static void main(String[] args) {
+        //411
+        System.out.println("aaa");
+        System.out.println("bbbb");
+    }
 }
